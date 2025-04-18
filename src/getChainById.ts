2e7a1901aa6f078c3b3322eee3ac447931400920
yjgaia <yjdev88@gmail.com>
2025-04-18 14:03:39 +0900

    fix: update package name format and improve type definitions
    - Changed package name from "@common-module/wallet-utils" to "@commonmodule/wallet-utils" in package.json.
    - Enhanced TypeScript definitions in getChainById.ts to return Chain or undefined.
    - Updated source maps for getChainById.d.ts and getChainById.js to reflect changes in mappings.

--- src/getChainById.ts
+++ src/getChainById.ts
@@ -1,7 +1,8 @@
 import * as all from "viem/chains";
+import { Chain } from "viem/chains";
 
 const { ...chains } = all;
-export default function getChainById(chainId: number) {
+export default function getChainById(chainId: number): Chain | undefined {
   for (const chain of Object.values(chains)) {
     if (chain.id === chainId) {
       return chain;
