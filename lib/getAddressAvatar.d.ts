de0a32b5440a3d7ac76f33781bd95bc73de1daca
yjgaia <yjdev88@gmail.com>
2025-05-13 13:56:11 +0900

    feat: add getAddressAvatar function and update exports

---
+++ lib/getAddressAvatar.d.ts
@@ -0,0 +1,2 @@
+export default function getAddressAvatar(address: string): string;
+//# sourceMappingURL=getAddressAvatar.d.ts.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
 export { default as AddressUtils } from "./AddressUtils.js";
+export { default as getAddressAvatar } from "./getAddressAvatar.js";
 export { default as getChainById } from "./getChainById.js";
